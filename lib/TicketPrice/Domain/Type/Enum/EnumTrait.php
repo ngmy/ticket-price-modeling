1fe91b0efdce233a720d47a149c5d45dea7365b7
Yuta Nagamiya <y.nagamiya@gmail.com>
2019-07-20 21:46:53 +0900

    例外メッセージを変更

--- lib/TicketPrice/Domain/Type/Enum/EnumTrait.php
+++ lib/TicketPrice/Domain/Type/Enum/EnumTrait.php
@@ -13,15 +13,6 @@ trait EnumTrait
      */
     private $scalar;
 
-    final public function __construct($value)
-    {
-        if (!self::isValidValue($value)) {
-            throw new InvalidArgumentException("不正な値です。(値='$value')");
-        }
-
-        $this->scalar = $value;
-    }
-
     final public static function isValidValue($value): bool
     {
         return in_array($value, self::ENUM, true);
@@ -35,10 +26,21 @@ trait EnumTrait
     final public static function __callStatic(string $name, array $args)
     {
         if (!self::isValidName($name)) {
-            throw new BadMethodCallException("不正な名前です。 (名前='{$name}')");
+            throw new BadMethodCallException('不正な名前です: ' .
+                var_export([self::ENUM, $name, $args], true));
         }
 
         return new self(self::ENUM[$name]);
+    }
+
+    final public function __construct($value)
+    {
+        if (!self::isValidValue($value)) {
+            throw new InvalidArgumentException('不正な値です: ' .
+                var_export([self::ENUM, $this, $value], true));
+        }
+
+        $this->scalar = $value;
     }
 
     final public function __toString(): string
@@ -53,6 +55,7 @@ trait EnumTrait
 
     final public function __set(string $name, $value): void
     {
-        throw new BadMethodCallException('全てのセッターは禁止されています。');
+        throw new BadMethodCallException('全てのセッターは禁止されています: ' .
+            var_export([self::ENUM, $this, $name, $value], true));
     }
 }
